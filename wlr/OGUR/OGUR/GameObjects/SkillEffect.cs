4c3591c5b0f15ca47f77f97d2435af1e64be7192
XBigTK13X <xbigtk13x@gmail.com>
2011-08-01 09:42:23 -0400

    Added some logic to handle the three different animations skills will need to support.

--- wlr/OGUR/OGUR/GameObjects/SkillEffect.cs
+++ wlr/OGUR/OGUR/GameObjects/SkillEffect.cs
@@ -11,6 +11,14 @@ namespace OGUR.GameObjects
 {
     public class SkillEffect:GameplayObject
     {
+
+        public enum Anim
+        {
+            RANGED,
+            SELF,
+            CLOUD
+        }
+
         private const float m_strengthDecayAmount = .75f;
         public const float DefaultStrength = 1;
         
@@ -19,9 +27,10 @@ namespace OGUR.GameObjects
         private readonly ICreature m_source;
         private readonly ISkill m_skill;
         private SpriteType m_spriteType;
+        private Anim m_animation;
 
 
-        public SkillEffect(float gridX, float gridY,Point2 velocity,ICreature source,ISkill skill,SpriteType sprite=SpriteType.EMPTY,float strength=DefaultStrength)
+        public SkillEffect(float gridX, float gridY,Point2 velocity,ICreature source,ISkill skill,SpriteType sprite=SpriteType.EMPTY,float strength=DefaultStrength,Anim animation = Anim.RANGED)
         {
             m_spriteType = sprite;
             Initialize((int)gridX, (int)gridY, SpriteType.SKILL_EFFECT, GameObjectType.SKILL_EFFECT);
@@ -29,6 +38,7 @@ namespace OGUR.GameObjects
             m_velocity = velocity;
             m_source = source;
             m_skill = skill;
+            m_animation = animation;
         }
 
         public override void Update()
@@ -39,17 +49,50 @@ namespace OGUR.GameObjects
             }
             else
             {
-                var collidedTarget = m_source.GetTargets().GetCollidedTarget(this);
-                if(null!=collidedTarget)
+                switch(m_animation)
                 {
-                    m_skill.Affect(collidedTarget);
-                    m_isActive = false;
-                    return;
+                    case Anim.CLOUD:
+                        CloudAnimation();
+                        break;
+                    case Anim.RANGED:
+                        RangedAnimation();
+                        break;
+                    case Anim.SELF:
+                        SelfAnimation();
+                        break;
+                    default:
+                        break;
                 }
-                m_strength *= m_strengthDecayAmount;
-                m_velocity.X *= m_strength;
-                m_velocity.Y *= m_strength;
-                Move(m_velocity.X,m_velocity.Y);
+            }
+        }
+
+        private void RangedAnimation()
+        {
+            AffectTouchingTargets();
+            m_strength *= m_strengthDecayAmount;
+            m_velocity.X *= m_strength;
+            m_velocity.Y *= m_strength;
+            Move(m_velocity.X,m_velocity.Y);
+        }
+
+        private void CloudAnimation()
+        {
+            AffectTouchingTargets();
+        }
+
+        private void SelfAnimation()
+        {
+            AffectTouchingTargets();
+        }
+
+        private void AffectTouchingTargets()
+        {
+            var collidedTarget = m_source.GetTargets().GetCollidedTarget(this);
+            if (null != collidedTarget)
+            {
+                m_skill.Affect(collidedTarget);
+                m_isActive = false;
+                return;
             }
         }
     }
