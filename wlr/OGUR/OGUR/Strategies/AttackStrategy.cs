b5fde91be3d16830a2f08f15d0c709e98f14114b
XBigTK13X <xbigtk13x@gmail.com>
2012-02-20 01:36:36 -0500

    Moved reusable pathfinding logic from AttackStrategy into IStrategy.

--- wlr/OGUR/OGUR/Strategies/AttackStrategy.cs
+++ wlr/OGUR/OGUR/Strategies/AttackStrategy.cs
@@ -18,81 +18,24 @@ namespace OGUR.Strategies
             }
         }
 
-        private ICreature opponent;
-        private readonly Path targetPath = new Path();
-        private readonly Point2 nextMove = new Point2(0, 0);
-        private const int throttleMin = 10;
-        private const int throttleMax = 20;
-        private int throttle = 0;
-        private static readonly Random rand = new Random();
-
         public override void Act()
         {
-            throttle--;
-            if (throttle <= 0)
+            if (AbleToMove())
             {
-                opponent = m_targets.FindClosest();
-                //Every player is dead
-                if (null != opponent)
+                if (SkillFactory.IsSkill(m_parent.GetActiveSkillName(), AnimationType.RANGED))
                 {
-                    targetPath.Copy(PathFinder.FindNextMove(m_parent.GetLocation(), opponent.GetLocation()));
-                }
-                throttle = rand.Next(throttleMin, throttleMax);
-            }
-            if (null != targetPath)
-            {
-                if (m_parent.Get(StatType.MOVE_COOL_DOWN) >= m_parent.GetMax(StatType.MOVE_COOL_DOWN))
-                {
-                    if (SkillFactory.IsSkill(m_parent.GetActiveSkillName(), AnimationType.RANGED))
+                    m_parent.SetSkillVector(CalculateTargetVector(m_parent.GetLocation(), opponent.GetLocation()));
+                    if (m_parent.GetSkillVector().GridX != 0 || m_parent.GetSkillVector().GridY != 0)
                     {
-                        m_parent.SetSkillVector(CalculateTargetVector(m_parent.GetLocation(), opponent.GetLocation()));
-                        if (m_parent.GetSkillVector().GridX != 0 || m_parent.GetSkillVector().GridY != 0)
-                        {
-                            m_parent.UseActiveSkill();
-                        }
-                    }
-                    if (targetPath.HasMoves())
-                    {
-                        nextMove.Copy(targetPath.GetNextMove());
-                        m_parent.MoveTo(nextMove);
+                        m_parent.UseActiveSkill();
                     }
                 }
-            }
-        }
-
-        private readonly Point2 diff = new Point2(0,0);
-        private Point2 CalculateTargetVector(Point2 source, Point2 dest)
-        {
-            diff.SetX(source.GridX - dest.GridX);
-            diff.SetY(source.GridY - dest.GridY);
-            if (diff.GridY == 0)
-            {
-                if (diff.GridX > 0)
+                if (targetPath.HasMoves())
                 {
-                    diff.SetX(-1f);
-                }
-                else
-                {
-                    diff.SetX(1f);
+                    nextMove.Copy(targetPath.GetNextMove());
+                    m_parent.MoveTo(nextMove);
                 }
             }
-            else if (diff.GridX == 0)
-            {
-                if (diff.GridY > 0)
-                {
-                    diff.SetY(-1f);
-                }
-                else
-                {
-                    diff.SetY(1f);
-                }
-            }
-            else
-            {
-                diff.SetX(0);
-                diff.SetY(0);
-            }
-            return diff;
         }
     }
 }
--- wlr/OGUR/OGUR/Strategies/IStrategy.cs
+++ wlr/OGUR/OGUR/Strategies/IStrategy.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using OGUR.Creatures;
+using OGUR.Collision;
+using OGUR.Paths;
+using System;
 
 namespace OGUR.Strategies
 {
@@ -9,6 +12,15 @@ namespace OGUR.Strategies
         protected TargetSet m_targets;
         protected ICreature m_parent;
         
+        protected const int throttleMin = 10;
+        protected const int throttleMax = 20;
+        protected int throttle = 0;
+
+        protected ICreature opponent;
+        protected readonly Path targetPath = new Path();
+        protected readonly Point2 nextMove = new Point2(0, 0);
+        protected static readonly Random rand = new Random();
+
         protected IStrategy(ICreature parent)
         {
             m_targets = new TargetSet(parent);
@@ -23,6 +35,64 @@ namespace OGUR.Strategies
         public void AddTargets(ICreature source)
         {
             m_targets.AddTargets(source);
+        }
+
+        protected bool AbleToMove()
+        {
+            throttle--;
+            if (throttle <= 0)
+            {
+                opponent = m_targets.FindClosest();
+                //Every player is dead
+                if (null != opponent)
+                {
+                    targetPath.Copy(PathFinder.FindNextMove(m_parent.GetLocation(), opponent.GetLocation()));
+                }
+                throttle = rand.Next(throttleMin, throttleMax);
+            }
+            if (null != targetPath)
+            {
+                if (m_parent.Get(StatType.MOVE_COOL_DOWN) >= m_parent.GetMax(StatType.MOVE_COOL_DOWN))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected readonly Point2 diff = new Point2(0, 0);
+        protected Point2 CalculateTargetVector(Point2 source, Point2 dest)
+        {
+            diff.SetX(source.GridX - dest.GridX);
+            diff.SetY(source.GridY - dest.GridY);
+            if (diff.GridY == 0)
+            {
+                if (diff.GridX > 0)
+                {
+                    diff.SetX(-1f);
+                }
+                else
+                {
+                    diff.SetX(1f);
+                }
+            }
+            else if (diff.GridX == 0)
+            {
+                if (diff.GridY > 0)
+                {
+                    diff.SetY(-1f);
+                }
+                else
+                {
+                    diff.SetY(1f);
+                }
+            }
+            else
+            {
+                diff.SetX(0);
+                diff.SetY(0);
+            }
+            return diff;
         }
     }
 }
