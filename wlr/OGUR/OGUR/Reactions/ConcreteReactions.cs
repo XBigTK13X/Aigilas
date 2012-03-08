aa549d36c812d25a2748dacfd4f656ea9f47514e
XBigTK13X <xbigtk13x@gmail.com>
2012-03-08 00:18:52 -0500

    Added calls to statuses that should work well for dual element reactions.

--- wlr/OGUR/OGUR/Reactions/ConcreteReactions.cs
+++ wlr/OGUR/OGUR/Reactions/ConcreteReactions.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using OGUR.Creatures;
+using OGUR.Statuses;
+using OGUR.Items;
 
 namespace OGUR.Reactions
 {
@@ -25,156 +27,182 @@ namespace OGUR.Reactions
     {
         public void Affect(ICreature target)
         {
+            throw new NotImplementedException();
         }
     }
     public class ScorchReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.WeakKnees);
         }
     }
     public class BlindReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.Blind);
         }
     }
     public class LacticAcidReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.WeakMuscles);
         }
     }
     public class MindBlownReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            target.ApplyDamage(10, null, true, StatType.MANA);
         }
     }
     public class VentReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.SlowDown);
         }
     }
     public class DrownReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.Mute);
         }
     }
     public class ReflectReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.HealReflect);
         }
     }
     public class DrenchReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.SoakingWet);
         }
     }
     public class PneumoniaReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.Poison);
         }
     }
     public class LobotomyReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.IntDown);
         }
     }
     public class RustReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.FragileItems);
         }
     }
     public class PurifyReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.PreventDarkUsage);
         }
     }
     public class EclipseReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.PreventLightUsage);
         }
     }
     public class RespectReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.Mutiny);
         }
     }
     public class CraftsmanReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            ItemFactory.CreateRandomPlain(target.GetLocation());
         }
     }
     public class FlashReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            throw new NotImplementedException();
         }
     }
     public class MetabolismReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.PreventRegeneration);
         }
     }
     public class FastForwardReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            target.ApplyDamage(-1, null, false, StatType.AGE);
         }
     }
     public class BlankReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.PreventMentalUsage);
         }
     }
     public class YinYangReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.SelfMutilation);
         }
     }
     public class ExposeReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.Flee);
         }
     }
     public class EnlightenReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            throw new NotImplementedException();
         }
     }
     public class AtrophyReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.WeakenStrength);
         }
     }
     public class NeurosisReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.LockSkillCycle);
         }
     }
     public class ConfuseReaction : IReaction
     {
         public void Affect(ICreature target)
         {
+            StatusFactory.Apply(target, Status.Confusion);
         }
     }
 
