dcf8ce6a78dd4b7675632f4b012a21171eba9171
XBigTK13X <xbigtk13x@gmail.com>
2012-05-20 22:37:36 -0400

    Stubbing out the logic for saving/loading games.

---
+++ wlr/SPX.Save/Save.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Storage;
+
+//http://stackoverflow.com/questions/3723287/good-example-of-xna-4-0-to-save-game-data
+
+namespace SPX.Saves
+{
+    public static class Save<T>
+    {
+        public static void Write(string fileName,T serializableSaveObject)
+        {
+
+        }
+
+        public static T Read(string fileName)
+        {
+            return (T)new Object();
+        }
+    }
+}
