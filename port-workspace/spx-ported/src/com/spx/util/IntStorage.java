ff8057ce17290ba23ee3e0c075a57690c4c4f91b
XBigTK13X <xbigtk13x@gmail.com>
2012-08-12 09:36:21 -0400

    #21: Ported spx.util

---
+++ port-workspace/spx-ported/src/com/spx/util/IntStorage.java
@@ -0,0 +1,14 @@
+package com.spx.util;import java.util.*;
+    public class IntStorage
+    {
+        private static HashMap<Float, Integer> __slots = new HashMap<Float, Integer>();
+
+        public static int Get(float value)
+        {
+            if (!__slots.containsKey(value))
+            {
+                __slots.put(value, (int)value);
+            }
+            return __slots.get(value);
+        }
+    }
---
+++ port-workspace/spx-ported/src/com/spx/util/StringSquisher.java
@@ -0,0 +1,35 @@
+package com.spx.util;import com.spx.wrapper.*;import java.util.*;
+
+    public class StringSquisher
+    {
+        private static StringBuilder builder = new StringBuilder(64);
+        private static HashMap<StringBuilder, String> lookup = new HashMap<StringBuilder, String>();
+
+        public static String Build(String...textToSquish)
+        {
+            Clear();
+            for (int ii = 0; ii < textToSquish.length; ii++)
+            {
+                builder.append(textToSquish[ii]);
+            }
+            return Flush();
+        }
+
+        public static void Squish(String...textToSquish)
+        {
+            for (int ii = 0; ii < textToSquish.length; ii++)
+            {
+                builder.append(textToSquish[ii]);
+            }
+        }
+
+        public static void Clear()
+        {
+            builder.setLength(0);
+        }
+
+        public static String Flush()
+        {
+            return builder.toString();
+        }
+    }
