445dfa2993b34b4579ba23470af93428f5ca5e06
XBigTK13X <xbigtk13x@gmail.com>
2012-08-14 21:14:32 -0400

    #21: ported everything except for concrete game components.

---
+++ port-workspace/aigilas-ported/src/com/aigilas/skills/SkillId.java
@@ -0,0 +1,123 @@
+package com.aigilas.skills;
+
+public class SkillId
+{
+    public static String[] Values = 
+    {
+        SkillId.ABSORB,
+        SkillId.ACID_DRIP,
+        SkillId.ACID_NOZZLE,
+        SkillId.BRIMSTONE,
+        SkillId.BOIL,
+        SkillId.BREAKING_WHEEL,
+        SkillId.CAVALRY,
+        SkillId.COLD_SHOULDER,
+        SkillId.COMBUST,
+        SkillId.CONFUSION,
+        SkillId.DART,
+        SkillId.DART_TRAP,
+        SkillId.ELECTRIFY,
+        SkillId.ENVENOM,
+        SkillId.EXPLODE,
+        SkillId.FIREBALL,
+        SkillId.FLAME_HAMMER,
+        SkillId.FLOOR_SPIKES,
+        SkillId.FORGET_SKILL,
+        SkillId.GUSH,
+        SkillId.HORDER,
+        SkillId.HORRIFY,
+        SkillId.ICE_SHARD,
+        SkillId.MAGIC_MAP,
+        SkillId.MANA_UP,
+        SkillId.MIMIC,
+        SkillId.MUTINY,
+        SkillId.NO_SKILL,
+        SkillId.POISON_CLOUD,
+        SkillId.PLAGUE,
+        SkillId.REGEN_ALL,
+        SkillId.REMOTE_MINE,
+        SkillId.SERPENT_SUPPER,
+        SkillId.SOUL_CRUSH,
+        SkillId.SOUL_REINFORCEMENT,
+        SkillId.SPAWN_ITEM,
+        SkillId.SPEED_UP,
+        SkillId.STEAL_ITEM,
+        SkillId.STRENGTH_UP,
+        SkillId.THROW_ITEM,
+        SkillId.VALEDICTORIAN,
+        SkillId.VAPOR_IMPLANT,
+        SkillId.VENOM_FIST,
+        SkillId.WALL_PUNCH,
+        SkillId.WEAK_KNEEES,
+        SkillId.VAPOR_CLOUD,
+        SkillId.DISMEMBERMENT,
+        SkillId.HYPOTHERMIA
+    };
+
+    public static final String FIREBALL = "Fireball";
+    public static final String NO_SKILL = "No Skill";
+    //Sloth Acolyte
+    public static final String FLOOR_SPIKES = "Floor Spikes";
+    public static final String DART = "Dart";
+    public static final String DART_TRAP = "Dart Trap";
+    public static final String ACID_DRIP = "Acid Drip";
+    public static final String ACID_NOZZLE = "Acid Nozzle";
+    public static final String REMOTE_MINE = "Remote Mine";
+    public static final String VAPOR_IMPLANT = "Vapor Implant";
+    //Envy Acolyte
+    public static final String CONFUSION = "Confusion";
+    public static final String WEAK_KNEEES = "Weak Knees";
+    public static final String VENOM_FIST = "Venom Fist";
+    public static final String ABSORB = "Absorb";
+    public static final String MUTINY = "Mutiny";
+    //Greed Acolyte
+    public static final String SOUL_REINFORCEMENT = "Soul Reinforcement";
+    public static final String HORDER = "Horder";
+    public static final String SPAWN_ITEM = "Drop Rate (+)";
+    public static final String THROW_ITEM = "Throw Item";
+    public static final String STEAL_ITEM = "Steal Item";
+    //Wrath Acolyte
+    public static final String FLAME_HAMMER = "Flame Hammer";
+    public static final String GUSH = "Gush";
+    public static final String SOUL_CRUSH = "Soul Crush";
+    public static final String COMBUST = "Combust";
+    public static final String HORRIFY = "Horrify";
+    //Gluttony Acolyte
+    public static final String FORGET_SKILL = "Forget Skill";
+
+
+    //Lust Acolyte
+    public static final String REGEN_ALL = "Regen All";
+    public static final String SPEED_UP = "Speed (+) All";
+    public static final String ENVENOM = "Envenom";
+    public static final String MAGIC_MAP = "Magic Map";
+    public static final String COLD_SHOULDER = "Cold Shoulder";
+    public static final String CAVALRY = "Cavalry";
+    //Pride Acolyte
+    public static final String STRENGTH_UP = "Strength (+)";
+    public static final String MANA_UP = "Mana (+)";
+    public static final String ELECTRIFY = "Electrify";
+    public static final String WALL_PUNCH = "Wall Punch";
+    public static final String MIMIC = "Mimic";
+    public static final String VALEDICTORIAN = "Valedictorian";
+
+    public static final String EXPLODE = "Explode";
+    public static final String VAPOR_CLOUD = "Vapor Cloud";
+
+    //Wrath
+    public static final String DISMEMBERMENT = "Dismemberment";
+    //Envy
+    public static final String HYPOTHERMIA = "Hypothermia";
+    public static final String ICE_SHARD = "Ice Shard";
+    //Gluttony
+    public static final String PLAGUE = "Plague";
+    public static final String POISON_CLOUD = "Poison Cloud";
+    //Sloth
+    public static final String SERPENT_SUPPER = "Serpent Supper";
+    //Pride
+    public static final String BREAKING_WHEEL = "Breaking Wheel";
+    //Greed
+    public static final String BOIL = "Boil";
+    //Lust
+    public static final String BRIMSTONE = "Brimstone";
+}
