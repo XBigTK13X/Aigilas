b6e7ac5a42e2dd2c13f2c1b8a23e8282590bf24e
XBigTK13X <xbigtk13x@gmail.com>
2012-08-15 22:18:10 -0400

    #21: Correcting some errors in the import resolution script.

--- script/fix_imports.py
+++ script/fix_imports.py
@@ -8,19 +8,19 @@ def isCodeFile(file):
 
 def fix_impl_imports(path):
 	for root,dirs,files in os.walk(path):
-		for dir in dirs:
-			print root+dir
+		for dir in dirs:			
 			fix_impl_imports(os.path.join(root,dir))
 		for file in files:
 			if isCodeFile(file):
+				print root+'/'+file
 				source = os.path.join(root,file)
 				result = os.path.join(root,file+"b")
 				r = open(source,'r')
 				w = open(result,'w')
 				for line in r.read().splitlines():
 					for impl in impls:
-						if impl in line and not 'package' in line:
-							missing = line.split(' ')[1].replace(impl,'')
+						if impl in line:
+							missing = 'import ' + line.split(' ')[1].replace(impl,'.*;')
 							line = line + '\r' + missing
 					w.write(line+'\r')
 				w.close()
