b6ad54f0c00fab5339fd41a6853f15aa1ad5d619
XBigTK13X <xbigtk13x@gmail.com>
2012-08-15 22:25:29 -0400

    #21: Fixing misnamed package.

--- script/fix_imports.py
+++ script/fix_imports.py
@@ -8,11 +8,9 @@ def isCodeFile(file):
 
 def fix_impl_imports(path):
 	for root,dirs,files in os.walk(path):
-		for dir in dirs:			
-			fix_impl_imports(os.path.join(root,dir))
 		for file in files:
 			if isCodeFile(file):
-				print root+'/'+file
+				print file
 				source = os.path.join(root,file)
 				result = os.path.join(root,file+"b")
 				r = open(source,'r')
