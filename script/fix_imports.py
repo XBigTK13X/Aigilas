7561789f9e74aa36513ce035b36c2af0b0759b42
XBigTK13X <xbigtk13x@gmail.com>
2012-08-15 22:49:27 -0400

    #21: More script tweaks.

--- script/fix_imports.py
+++ script/fix_imports.py
@@ -2,7 +2,7 @@ import os
 
 impls = ['.impl','.behaviors','.animations']
 targetPath = 'c:/_z/dev/git/aigilas/code/'
-reactionsPath = targetPath + '/aigilas-ported/com/aigilas/reactions/impl'
+reactionsPath = targetPath + '/aigilas-ported/src/com/aigilas/reactions/impl'
 
 def isCodeFile(file):
 	return '.java' in file
