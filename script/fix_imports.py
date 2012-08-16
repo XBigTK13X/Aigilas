be21bd6327109e6f0a9f6f7672eb08cbca014732
XBigTK13X <xbigtk13x@gmail.com>
2012-08-15 22:02:50 -0400

    #21: Correcting some errors in the import resolution script.

--- script/fix_imports.py
+++ script/fix_imports.py
@@ -1,15 +1,16 @@
 import os
 
 impls = ['.impl','.behaviors','.animations']
-targetPath = 'c:/_z/dev/git/aigilas/'
+targetPath = 'c:/_z/dev/git/aigilas/code/'
 
 def fix_impl_imports(path):
 	for root,dirs,files in os.walk(path):
 		for dir in dirs:
 			fix_impl_imports(os.path.join(root,dir))
 		for file in files:
-			source = os.join.path(root,file)
-			result = os.join.path(root,file+"b")
+			print file
+			source = os.path.join(root,file)
+			result = os.path.join(root,file+"b")
 			r = open(source,'r')
 			w = open(result,'w')
 			for line in r.read().splitlines():
@@ -17,7 +18,7 @@ def fix_impl_imports(path):
 					if impl in line:
 						missing = line.split(' ')[1].replace(impl,'')
 						line = line + '\r' + missing
-				w.write(line)
+				w.write(line='\r')
 			w.close()
 			r.close()
 			os.remove(source)
