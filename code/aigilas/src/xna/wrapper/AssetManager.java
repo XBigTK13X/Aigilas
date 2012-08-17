540c8bb50339b42796324a2dfb144e981b6bdd9f
XBigTK13X <xbigtk13x@gmail.com>
2012-08-16 22:08:36 -0400

    #21: More xna porting.

---
+++ code/aigilas/src/xna/wrapper/AssetManager.java
@@ -0,0 +1,40 @@
+package xna.wrapper;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+public class AssetManager {
+	private static final String assetPath = "assets";
+	private static AssetManager instance;
+
+	public static AssetManager Get() {
+		if (instance == null) {
+			instance = new AssetManager();
+		}
+		return instance;
+	}
+
+	private SpriteSheet gameplaySheet;
+
+	private static String Graphic(String fileName) {
+		return assetPath + "/graphics/" + fileName;
+	}
+
+	private AssetManager() {
+		try {
+			gameplaySheet = new SpriteSheet(Graphic("Gameplay.png"), 32, 32, 1);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Image GetSprite(String fileName) {
+		try {
+			return new Image(Graphic(fileName));
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
--- code/aigilas/src/xna/wrapper/Texture2D.java
+++
@@ -1,1 +0,0 @@
-package xna.wrapper;public class Texture2D {	public Rectangle Bounds;}
