1e8f641f350199b945c99168a2d4e337c99cb258
XBigTK13X <xbigtk13x@gmail.com>
2012-08-18 09:54:48 -0400

    #21: Replaced most of the rendering logic with Slick equivalents.

--- code/aigilas/src/xna/wrapper/GraphicsDevice.java
+++ code/aigilas/src/xna/wrapper/GraphicsDevice.java
@@ -1,1 +1,1 @@
-package xna.wrapper;public class GraphicsDevice {	public void clear(Color backgroundColor) {		// TODO Auto-generated method stub			}}
+package xna.wrapper;import org.newdawn.slick.Color;public class GraphicsDevice {	public void clear(Color backgroundColor) {		// TODO Auto-generated method stub	}}
