22309e3e76fe881fe201ebe4a36f89505a58a4f6
XBigTK13X <xbigtk13x@gmail.com>
2012-08-24 20:41:08 -0400

    #21: New script to help with the migration.

---
+++ script/add_import.py
@@ -0,0 +1,17 @@
+import os
+
+def fix(path):
+    for root,dirs,files in os.walk(path):
+        for file in files:
+            if ".java" in file:
+                convert_file = os.path.join(root,file)
+                shutil.copyfile(convert_file,convert_file+'b')
+                w = open(convert_file,'w')
+                for line in open(convert_file+'b','r').read().splitlines():
+                    if 'package ' in line:
+			line = line + '\r import com.badlogic.gdx.graphics.Color;\r'
+                    w.write(line)
+                w.close()
+                os.remove(convert_file+'b')
+
+fix('/home/dev/aigilas/code')
