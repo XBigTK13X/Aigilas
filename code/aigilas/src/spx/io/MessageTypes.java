206f1924ad225d238425840e64421608b8d81a53
XBigTK13X <xbigtk13x@gmail.com>
2012-08-27 17:05:59 -0400

    #21: Moved networking code into its own package.

--- code/aigilas/src/spx/io/MessageTypes.java
+++
@@ -1,1 +0,0 @@
-package spx.io;public enum MessageTypes {	CONNECT((byte) 1),	MOVEMENT((byte) 2),	START_GAME((byte) 3),	CHECK_STATE((byte) 4),	PLAYER_COUNT((byte) 5),	SYNC_STATE((byte) 6),	READY_FOR_NEXT_TURN((byte) 7),	HEART_BEAT((byte) 8);	private byte _value;	private MessageTypes(byte value) {		_value = value;	}	public byte getValue() {		return _value;	}}
---
+++ code/aigilas/src/spx/net/MessageTypes.java
@@ -0,0 +1,1 @@
+package spx.net;public enum MessageTypes {	CONNECT((byte) 1),	MOVEMENT((byte) 2),	START_GAME((byte) 3),	CHECK_STATE((byte) 4),	PLAYER_COUNT((byte) 5),	SYNC_STATE((byte) 6),	READY_FOR_NEXT_TURN((byte) 7),	HEART_BEAT((byte) 8);	private byte _value;	private MessageTypes(byte value) {		_value = value;	}	public byte getValue() {		return _value;	}}
