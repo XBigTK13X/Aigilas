69cc447dd800a4b8c40a4d119a9f096a1e4a18a2
XBigTK13X <xbigtk13x@gmail.com>
2012-09-09 01:43:41 -0400

    #22: Removing some unused code.

--- code/aigilas/src/spx/sprites/AnimatedTextureFactory.java
+++
@@ -1,1 +0,0 @@
-package spx.sprites; public class AnimatedTextureFactory {	public static AnimatedTexture Create(int type, int x, int y) {		AnimatedTexture sprite = new AnimatedTexture();		sprite.LoadContent(type);		sprite.SetPosition(x, y);		return sprite;	}}
