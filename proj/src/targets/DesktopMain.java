2953fb06d33cdd687386b3d3ba86320cbd989f31
XBigTK13X <xbigtk13x@gmail.com>
2012-09-16 18:17:49 -0400

    Reverting script changes.

---
+++ proj/src/targets/DesktopMain.java
@@ -0,0 +1,32 @@
+package targets;
+
+import aigilas.Aigilas;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import spx.core.Settings;
+import spx.core.SpxManager;
+import spx.net.Server;
+
+public class DesktopMain {
+    public static void main(String[] args) {
+
+        if (Settings.Get().networkingEnabled) {
+            Thread server = new Server();
+            server.start();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Launching the main game loop");
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Aigilas";
+        cfg.width = SpxManager.RenderWidth;
+        cfg.height = SpxManager.RenderHeight;
+        cfg.useGL20 = true;
+        new LwjglApplication(new Aigilas(), cfg);
+    }
+}
