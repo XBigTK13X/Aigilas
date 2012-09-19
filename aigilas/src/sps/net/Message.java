027412284d30d8b3c4935cb74ac19955ad1f636b
XBigTk13X <xbigtk13x@gmail.com>
2012-09-19 00:47:07 -0400

    #22: Messages are not generating the garbage. It is very likely due to both inefficiencies in my networking code and excess garbage collection by the JVM.

--- aigilas/src/sps/net/Message.java
+++ aigilas/src/sps/net/Message.java
@@ -7,10 +7,7 @@ import java.util.HashMap;
 
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
-
-    public static final int CommandMax = 16;
     public static final int PlayerMax = 4;
-    public static final int ByteCount = 80;
 
     private static final byte TrueByte = (byte) 1;
     private static final byte FalseByte = (byte) 0;
@@ -23,20 +20,20 @@ public class Message implements Serializable {
     public boolean IsActive;
     public Integer RngSeed;
     public byte PlayerCount;
-    public final byte[] PlayerOneState = new byte[16];
-    public final byte[] PlayerTwoState = new byte[16];
-    public final byte[] PlayerThreeState = new byte[16];
-    public final byte[] PlayerFourState = new byte[16];
+    public final byte[] PlayerOneState = new byte[Commands.values().length];
+    public final byte[] PlayerTwoState = new byte[Commands.values().length];
+    public final byte[] PlayerThreeState = new byte[Commands.values().length];
+    public final byte[] PlayerFourState = new byte[Commands.values().length];
 
-    private Message() {
+    protected Message() {
     }
 
     public static Message empty() {
-        return new Message();
+        return MessagePool.get();
     }
 
     public static Message create(MessageTypes messageType) {
-        Message result = new Message();
+        Message result = MessagePool.get();
         result.MessageType = messageType;
         return result;
     }
@@ -50,30 +47,22 @@ public class Message implements Serializable {
     }
 
     public static Message createPlayerCount(int playerCount) {
-        Message result = new Message();
+        Message result = MessagePool.get();
         result.MessageType = MessageTypes.PLAYER_COUNT;
         result.PlayerCount = (byte) playerCount;
         return result;
     }
 
     public static Message createInit(int playerCount, int rngSeed) {
-        Message result = new Message();
+        Message result = MessagePool.get();
         result.MessageType = MessageTypes.CONNECT;
         result.PlayerCount = (byte) playerCount;
         result.RngSeed = rngSeed;
         return result;
     }
 
-    public static Message createCheckState(Commands command, int playerIndex) {
-        Message result = new Message();
-        result.MessageType = MessageTypes.CHECK_STATE;
-        result.Command = command;
-        result.PlayerIndex = playerIndex;
-        return result;
-    }
-
     public static Message createMovement(Commands command, int playerIndex, boolean isActive) {
-        Message result = new Message();
+        Message result = MessagePool.get();
         result.MessageType = MessageTypes.MOVEMENT;
         result.PlayerIndex = playerIndex;
         result.Command = command;
@@ -82,7 +71,7 @@ public class Message implements Serializable {
     }
 
     public static Message createPlayerState(HashMap<Integer, HashMap<Commands, Boolean>> playerStatus, Integer turnCount, Integer rngSeed) {
-        Message result = new Message();
+        Message result = MessagePool.get();
         result.MessageType = MessageTypes.SYNC_STATE;
         result.writePlayerState(playerStatus);
         result.TurnCount = turnCount;
@@ -92,19 +81,19 @@ public class Message implements Serializable {
 
     public void writePlayerState(HashMap<Integer, HashMap<Commands, Boolean>> state) {
         for (int jj = 0; jj < PlayerMax; jj++) {
-            for (int ii = 0; ii < CommandMax; ii++) {
+            for (Commands command:Commands.values()) {
                 switch (jj) {
                     case 0:
-                        PlayerOneState[ii] = (state.get(jj).get(ii)) ? TrueByte : FalseByte;
+                        PlayerOneState[command.ordinal()] = (state.get(jj).get(command)) ? TrueByte : FalseByte;
                         break;
                     case 1:
-                        PlayerTwoState[ii] = (state.get(jj).get(ii)) ? TrueByte : FalseByte;
+                        PlayerTwoState[command.ordinal()] = (state.get(jj).get(command)) ? TrueByte : FalseByte;
                         break;
                     case 2:
-                        PlayerThreeState[ii] = (state.get(jj).get(ii)) ? TrueByte : FalseByte;
+                        PlayerThreeState[command.ordinal()] = (state.get(jj).get(command)) ? TrueByte : FalseByte;
                         break;
                     case 3:
-                        PlayerFourState[ii] = (state.get(jj).get(ii)) ? TrueByte : FalseByte;
+                        PlayerFourState[command.ordinal()] = (state.get(jj).get(command)) ? TrueByte : FalseByte;
                         break;
                 }
             }
@@ -113,19 +102,19 @@ public class Message implements Serializable {
 
     public void readPlayerState(HashMap<Integer, HashMap<Commands, Boolean>> result) {
         for (int jj = 0; jj < PlayerMax; jj++) {
-            for (int ii = 0; ii < CommandMax; ii++) {
+            for (Commands command: Commands.values()) {
                 switch (jj) {
                     case 0:
-                        result.get(jj).put(Commands.values()[ii], (PlayerOneState[ii] == TrueByte));
+                        result.get(jj).put(command, (PlayerOneState[command.ordinal()] == TrueByte));
                         break;
                     case 1:
-                        result.get(jj).put(Commands.values()[ii], (PlayerTwoState[ii] == TrueByte));
+                        result.get(jj).put(command, (PlayerTwoState[command.ordinal()] == TrueByte));
                         break;
                     case 2:
-                        result.get(jj).put(Commands.values()[ii], (PlayerThreeState[ii] == TrueByte));
+                        result.get(jj).put(command, (PlayerThreeState[command.ordinal()] == TrueByte));
                         break;
                     case 3:
-                        result.get(jj).put(Commands.values()[ii], (PlayerFourState[ii] == TrueByte));
+                        result.get(jj).put(command, (PlayerFourState[command.ordinal()] == TrueByte));
                         break;
                 }
             }
@@ -134,5 +123,10 @@ public class Message implements Serializable {
 
     public void clear() {
         MessageType = null;
+    }
+
+    public Message reset() {
+        clear();
+        return this;
     }
 }
---
+++ aigilas/src/sps/net/MessagePool.java
@@ -0,0 +1,20 @@
+package sps.net;
+
+import java.util.ArrayList;
+
+public class MessagePool extends Message{
+    private static ArrayList<Message> messages;
+    private static int index = 0;
+
+    public static Message get(){
+        if(messages == null){
+            messages = new ArrayList<>();
+            do{
+            messages.add(new Message());
+            }
+            while(messages.size()<100);
+        }
+        index = (index + 1) % messages.size();
+        return messages.get(index).reset();
+    }
+}
