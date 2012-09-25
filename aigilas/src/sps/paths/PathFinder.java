e2089cbd8fa41be37e3382afdc2a1e9c5a5f64a5
XBigTk13X <xbigtk13x@gmail.com>
2012-09-25 00:25:05 -0400

    #9: Continuing to simplify path finding.

--- aigilas/src/sps/paths/PathFinder.java
+++ aigilas/src/sps/paths/PathFinder.java
@@ -11,19 +11,54 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class PathFinder {
-    private static final PriorityQueue<Path> queue = new PriorityQueue<>(100, new Comparator<Path>() {
-        @Override
-        public int compare(Path o1, Path o2) {
-            return (int) (o1.getCost() - o2.getCost());
-        }
-    });
     private static Point2 node = new Point2(0, 0);
-    private static Path path;
     private static List<Point2> neighbors = new ArrayList<>();
 
+    private static PriorityQueue<Point2> open = new PriorityQueue<>();
+    private static ArrayList<Point2> closed = new ArrayList<>();
+    private static Path result = new Path();
+
     public static Path findNextMove(Point2 start, Point2 destination) {
-        queue.clear();
-        queue.add(PathFactory.create(start, destination));
+        open.clear();
+        closed.clear();
+        node.reset(0,0);
+        open.add(start);
+        while(!node.isSameSpot(destination)){
+            node = open.peek();
+            open.remove();
+            if(node.isSameSpot(destination)){
+                return new Path(closed);
+            }
+            closed.add(node);
+            for(Point2 neighbor:node.getNeighbors()){
+                neighbor.setWeight(node.Weight + 1);
+            }
+        }
+
+        while (we have not reached our goal) {
+            consider the best node in the open list (the node with the lowest f value)
+            if (this node is the goal) {
+                then we're done
+            }
+            else {
+                move the current node to the closed list and consider all of its neighbors
+                for (each neighbor) {
+                    if (this neighbor is in the closed list and our current g value is lower) {
+                        update the neighbor with the new, lower, g value
+                        change the neighbor's parent to our current node
+                    }
+                    else if (this neighbor is in the open list and our current g value is lower) {
+                        update the neighbor with the new, lower, g value
+                        change the neighbor's parent to our current node
+                    }
+                    else this neighbor is not in either the open or closed list {
+                        add the neighbor to the open list and set its g value
+                    }
+                }
+            }
+        }
+
+
         while (!queue.isEmpty()) {
             path = queue.peek();
             queue.remove(queue.peek());
