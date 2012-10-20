dc4395660a34e0ff24d9a85ba57b552dbdb4f824
XBigTK13X <xbigtk13x@gmail.com>
2012-10-20 15:32:33 -0400

    Removing some debugging output.

--- aigilas/src/sps/audio/DefaultMusicPlayer.java
+++ aigilas/src/sps/audio/DefaultMusicPlayer.java
@@ -3,14 +3,12 @@ package sps.audio;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.files.FileHandle;
-import sps.core.Logger;
 
 public class DefaultMusicPlayer extends MusicPlayer {
     private final Music mainTheme;
 
     public DefaultMusicPlayer() {
         FileHandle mainThemeFh = new FileHandle("assets/music/MainTheme.mp3");
-        Logger.info(mainThemeFh.file().getAbsolutePath());
         mainTheme = Gdx.audio.newMusic(mainThemeFh);
         mainTheme.setLooping(true);
     }
