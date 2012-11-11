5c3f958451a7e3fbe4be2762534386f65597e188
XBigTK13X <xbigtk13x@gmail.com>
2012-11-11 15:51:40 -0500

    Fix #88

--- aigilas/src/aigilas/skills/impl/SpeedUpSkill.java
+++ aigilas/src/aigilas/skills/impl/SpeedUpSkill.java
@@ -7,19 +7,13 @@ import aigilas.skills.SkillId;
 import aigilas.statuses.Status;
 
 public class SpeedUpSkill extends BaseSkill {
-    public SpeedUpSkill()
-
-    {
+    public SpeedUpSkill() {
         super(SkillId.Speed_Up, AnimationType.SELF);
-
-
     }
 
     @Override
     public void activate(BaseCreature source) {
 
         applyToPlayers(Status.SpeedUp);
-
     }
-
 }
