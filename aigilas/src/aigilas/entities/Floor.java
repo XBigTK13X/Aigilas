ccca00cec1a9f08585cba4c5d8c51630378e2ecf
XBigTK13X <xbigtk13x@gmail.com>
2012-11-13 22:40:46 -0500

    #62: Majority of the work required to allow for dynamic wall sprites. Still needs a little finessing to render properly.

--- aigilas/src/aigilas/entities/Floor.java
+++ aigilas/src/aigilas/entities/Floor.java
@@ -1,11 +1,13 @@
 package aigilas.entities;
 
+import aigilas.Common;
 import sps.bridge.DrawDepths;
 import sps.bridge.EntityTypes;
 import sps.bridge.SpriteTypes;
 import sps.core.Core;
 import sps.core.Point2;
 import sps.entities.Entity;
+import sps.entities.EntityManager;
 
 public class Floor extends Entity {
     public Floor(Point2 location) {
