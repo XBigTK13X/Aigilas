852ec68f205dbdf5bf29c98ab5ff85566c4e8c4a
XBigTK13X <xbigtk13x@gmail.com>
2012-12-04 22:24:23 -0500

    Close #63: A script to allow double click launch from Mac OS X. Would like something with more polish that can handle a missing JRE (for example), but these will do fine for now.

---
+++ dist/Start Aigilas (Mac OS X).command
@@ -0,0 +1,4 @@
+#! /bin/sh
+#
+cd "$(dirname "$0")"
+java -jar aigilas.jar > aigilas.log 2>&1
--- script/create_package.sh
+++ script/create_package.sh
@@ -9,5 +9,6 @@ ln ../../dist/README.txt ./README.txt
 cp -r ../assets .
 cp ../../dist/Start\ Aigilas\ \(Linux\).sh .
 cp ../../dist/Start\ Aigilas\ \(Windows\).bat .
-zip -r aigilas.zip aigilas.jar assets version.txt README.txt Start\ Aigilas\ \(Linux\).sh Start\ Aigilas\ \(Windows\).bat
+cp ../../dist/Start\ Aigilas\ \(Mac\ OS\ X\).command .
+zip -r aigilas.zip aigilas.jar assets version.txt README.txt Start\ Aigilas\ \(Linux\).sh Start\ Aigilas\ \(Windows\).bat Start\ Aigilas\ \(Mac\ OS\ X\).command
 cd ../..
