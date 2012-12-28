8025fe8d68ab26bbb586627c763f5c678a3e6d12
XBigTK13X <xbigtk13x@gmail.com>
2012-12-28 00:40:28 -0500

    Close #104

--- aigilas/src/aigilas/entities/Downstairs.java
+++ aigilas/src/aigilas/entities/Downstairs.java
@@ -3,9 +3,11 @@ package aigilas.entities;
 import aigilas.Common;
 import aigilas.creatures.BaseCreature;
 import aigilas.dungeons.Dungeon;
+import sps.bridge.ActorTypes;
 import sps.bridge.DrawDepths;
 import sps.bridge.EntityTypes;
 import sps.bridge.SpriteTypes;
+import sps.core.Core;
 import sps.core.Point2;
 import sps.entities.Entity;
 import sps.entities.EntityManager;
@@ -15,15 +17,31 @@ public class Downstairs extends Entity {
         initialize(location, SpriteTypes.get(Common.Downstairs), EntityTypes.get(Common.Downstairs), DrawDepths.get(Common.Stairs));
     }
 
+    boolean disabled = false;
     private BaseCreature player;
 
     @Override
     public void update() {
-        player = (BaseCreature) EntityManager.get().getTouchingCreature(this);
-        if (player != null) {
-            if (player.isInteracting()) {
-                player.performInteraction();
-                Dungeon.getNextFloor();
+        int nonPlayerCount = EntityManager.get().getActors(ActorTypes.get(Core.Non_Player)).size();
+        int dummyCount = EntityManager.get().getActors(ActorTypes.get(Common.Dummy)).size();
+        int minionCount = EntityManager.get().getActors(ActorTypes.get(Common.Minion)).size();
+
+        if (nonPlayerCount - dummyCount - minionCount > 0) {
+            disabled = true;
+            _graphic.setAlpha(0);
+        }
+        else {
+            _graphic.setAlpha(255);
+            disabled = false;
+        }
+
+        if (!disabled) {
+            player = (BaseCreature) EntityManager.get().getTouchingCreature(this);
+            if (player != null) {
+                if (player.isInteracting()) {
+                    player.performInteraction();
+                    Dungeon.getNextFloor();
+                }
             }
         }
     }
