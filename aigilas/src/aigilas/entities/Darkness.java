560482df286f3a8cf0e60b9bc4606e1ded0ef4a6
XBigTK13X <xbigtk13x@gmail.com>
2013-01-10 20:42:54 -0500

    #39: Min style the acolytes.

--- aigilas/src/aigilas/entities/Darkness.java
+++ aigilas/src/aigilas/entities/Darkness.java
@@ -5,6 +5,7 @@ import sps.bridge.DrawDepths;
 import sps.bridge.EntityTypes;
 import sps.bridge.SpriteTypes;
 import sps.core.Point2;
+import sps.core.RNG;
 import sps.core.Settings;
 import sps.entities.Entity;
 import sps.entities.EntityManager;
@@ -15,6 +16,7 @@ public class Darkness extends Entity {
 
     public Darkness(Point2 location) {
         initialize(location, SpriteTypes.get(Common.Entities.Darkness), EntityTypes.get(Common.Entities.Darkness), DrawDepths.get(Common.Entities.Darkness));
+        transparency = RNG.next(30,70)/100f;
     }
 
     boolean playerNear;
