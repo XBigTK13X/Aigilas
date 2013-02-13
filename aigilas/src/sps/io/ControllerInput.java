034ab49035c0d6a0f09bafc161c6798412a9dba6
XBigTK13X <xbigtk13x@gmail.com>
2013-02-13 17:40:32 -0500

    BROKEN #3: Defining a shema for controller input serialization to and from config

--- aigilas/src/sps/io/ControllerInput.java
+++ aigilas/src/sps/io/ControllerInput.java
@@ -11,6 +11,25 @@ public class ControllerInput {
     private Integer axis;
     private Boolean positive;
     private Boolean nonZero;
+    private Device device;
+
+    private ControllerInput(Integer buttonIndex, Integer axisIndex, Integer povIndex, PovDirection direction, Boolean nonzero, Boolean positive) {
+        pov = povIndex;
+        povDirection = direction;
+        axis = axisIndex;
+        button = buttonIndex;
+        this.positive = positive;
+        nonZero = nonzero;
+        if (pov != null) {
+            device = Device.Pov;
+        }
+        else if (axis != null) {
+            device = Device.Axis;
+        }
+        else {
+            device = Device.Button;
+        }
+    }
 
     public static ControllerInput createButton(int index) {
         return new ControllerInput(index, null, null, null, null, null);
@@ -32,13 +51,39 @@ public class ControllerInput {
         return new ControllerInput(null, null, index, direction, null, null);
     }
 
-    private ControllerInput(Integer buttonIndex, Integer axisIndex, Integer povIndex, PovDirection direction, Boolean nonzero, Boolean positive) {
-        pov = povIndex;
-        povDirection = direction;
-        axis = axisIndex;
-        button = buttonIndex;
-        this.positive = positive;
-        nonZero = nonzero;
+    public static ControllerInput parse(String source) {
+        String[] parts = source.split("/");
+        Device device = Device.get(parts[0]);
+        int index = Integer.parseInt(parts[1]);
+        switch (device) {
+            case Button:
+                return createButton(index);
+            case Pov:
+                int povIndex = index;
+                PovDirection direction = getDirection(parts[2]);
+                return createPov(povIndex, direction);
+            case Axis:
+                String trigger = parts[2];
+                if (trigger.equalsIgnoreCase("nonzero")) {
+                    return createNonZeroAxis(index);
+                }
+                else if (trigger.equalsIgnoreCase("positive")) {
+                    return createPositiveAxis(index);
+                }
+                else {
+                    return createNegativeAxis(index);
+                }
+        }
+        return null;
+    }
+
+    private static PovDirection getDirection(String name) {
+        for (PovDirection direction : PovDirection.values()) {
+            if (direction.name().equalsIgnoreCase(name)) {
+                return direction;
+            }
+        }
+        return null;
     }
 
     public boolean isActive(Controller controller) {
@@ -63,14 +108,37 @@ public class ControllerInput {
         return controller.getPov(pov) == povDirection;
     }
 
-    public static ControllerInput parse(String source) {
-        ControllerInput result = null;
-
+    public String serialize() {
+        String result = "";
+        result += device.name() + "/";
+        switch (device) {
+            case Button:
+                result += button;
+                break;
+            case Pov:
+                result += pov + "/";
+                result += povDirection.name();
+                break;
+            case Axis:
+                result += axis + "/";
+                result += (positive == null) ? "NonZero" : (positive) ? "Positive" : "Negative";
+                break;
+        }
         return result;
     }
 
-    public String serialize() {
-        String result = "";
-        return result;
+    private enum Device {
+        Button,
+        Pov,
+        Axis;
+
+        public static Device get(String name) {
+            for (Device device : values()) {
+                if (device.name().equalsIgnoreCase(name)) {
+                    return device;
+                }
+            }
+            return null;
+        }
     }
 }
