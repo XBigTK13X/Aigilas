d2ac1a74e3729a47c9662e9948248f1da6a2a3b7
XBigTK13X <xbigtk13x@gmail.com>
2013-05-18 07:52:19 -0400

    Adding unstable build step.

--- script/create_package.sh
+++ script/create_package.sh
@@ -19,4 +19,9 @@ git log --max-count=1 > pkg/assets/data/git.txt
 echo "== Creating the archive: pkg/aigilas.zip"
 cd pkg
 zip -rq aigilas.zip ./* -x "*.DS_Store"
+echo "=== Present working dir is"
+pwd
+echo "=== Correcting permissions."
+cp pkg/aigilas.zip pkg/aigilas_unstable_edition.zip
+chmod 755 pkg/aigilas_unstable_edition.zip
 cd ..
