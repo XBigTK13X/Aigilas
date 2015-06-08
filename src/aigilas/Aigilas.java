3a8c8c229d4cf5cdf2fbb35a89f1d067f4e14c83
papillon88 <devopreet.ch@gmail.com>
2015-06-08 16:31:56 -0500

    testcommit1

--- src/aigilas/Aigilas.java
+++ src/aigilas/Aigilas.java
@@ -24,6 +24,7 @@ public class Aigilas implements ApplicationListener {
 
     @Override
     public void create() {
+    	//comments
         Sps.setup();
         Renderer.get().setWindowsBackground(Color.BLACK);
         Renderer.get().setStrategy(new FrameStrategy());
