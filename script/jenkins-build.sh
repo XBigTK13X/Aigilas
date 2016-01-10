457d886dbbd96c313c1b8406e02d5bbca0d08816
XBigTK13X <xbigtk13x@gmail.com>
2016-01-10 10:26:12 -0600

    Jenkins script

--- script/jenkins-build.sh
+++ script/jenkins-build.sh
@@ -3,4 +3,5 @@
 script/prepare_package.sh
 cd $JENKINS_HOME/tools/conyay
 /bin/bash -c "./power.sh Aigilas $WORKSPACE/pkg"
+rm -rf $WORKSPACE/artifacts || true
 mv ./rap/Aigilas/packages $WORKSPACE/artifacts
