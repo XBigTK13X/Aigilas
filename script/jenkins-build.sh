cb41eb8c721d5d8b4713420e285fef9b63c7acdf
XBigTK13X <xbitk13x@gmail.com>
2016-06-29 10:03:52 -0500

    itch.io butler push as part of the Jenkins build

--- script/jenkins-build.sh
+++ script/jenkins-build.sh
@@ -5,3 +5,14 @@ cd $JENKINS_HOME/tools/conyay
 /bin/bash -c "./power.sh Aigilas $WORKSPACE/pkg"
 rm -rf $WORKSPACE/artifacts || true
 mv ./rap/Aigilas/packages $WORKSPACE/artifacts
+
+push(){
+	ZIP=$1
+	CHANNEL=$2
+	butler push $WORKSPACE/artifacts/$ZIP simplepathstudios/aigilas $CHANNEL --userversion-file $WORKSPACE/assets/data/version.dat
+}
+
+push Aigilas-lin-32.zip linux-stable-32
+push Aigilas-lin-64.zip linux-stable-64
+push Aigilas-mac.zip osx-stable
+push Aigilas-win-32.zip windows-stable-32
