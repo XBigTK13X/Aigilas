9afe9c863ea80a72a59288e0acc7f5a02bb64081
XBigTK13X <xbitk13x@gmail.com>
2016-06-29 10:09:56 -0500

    Fix itch.io butler config

--- script/jenkins-build.sh
+++ script/jenkins-build.sh
@@ -9,10 +9,9 @@ mv ./rap/Aigilas/packages $WORKSPACE/artifacts
 push(){
 	ZIP=$1
 	CHANNEL=$2
-	butler push $WORKSPACE/artifacts/$ZIP simplepathstudios/aigilas $CHANNEL --userversion-file $WORKSPACE/assets/data/version.dat
+	butler push $WORKSPACE/artifacts/$ZIP simplepathstudios/aigilas:$CHANNEL --userversion-file $WORKSPACE/assets/data/version.dat
 }
 
-push Aigilas-lin-32.zip linux-stable-32
-push Aigilas-lin-64.zip linux-stable-64
+push Aigilas-lin-64.zip linux-stable
 push Aigilas-mac.zip osx-stable
-push Aigilas-win-32.zip windows-stable-32
+push Aigilas-win-32.zip windows-stable
